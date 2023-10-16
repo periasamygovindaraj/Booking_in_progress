91834b3db134da36d459051861566f44a26665b2
periasamygovindaraj <102314168+periasamygovindaraj@users.noreply.github.com>
2023-10-16 11:04:43 +0530

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -6,13 +6,13 @@ public class Customer {
 	char pickup_point;
 	char drop_point;
 	int pickup_time;
-	
+	//test
 	
 	public Customer(int customre_Id,char pickup_point,char drop_point,int pickup_time) {
 		this.customer_Id=customre_Id;
 		this.pickup_point=pickup_point;
 		this.drop_point=drop_point;
 		this.pickup_time=pickup_time;
-		
+		//test
 	}
 }
